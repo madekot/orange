817426433ae270b6f6278791daf1c6d1e44dba33
Михаил Голубцов <mgolubcov@gmail.com>
2020-09-29 13:45:29 +0300

    изменения

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ const gulp = require('gulp');
 const pug = require('gulp-pug');
 
 gulp.task('pug-compile', ()=>{
-  return gulp.src(['source/pug/**/*.pug', '!app/pug/includes/**/*.pug'])
+  return gulp.src(['source/pug/**/*.pug', '!source/pug/includes/**/*.pug'])
     .pipe(pug({pretty:true}))
-    .pipe(gulp.dest('source/html'))
+    .pipe(gulp.dest('./build'))
 });
 
 gulp.task('watch',()=>{
